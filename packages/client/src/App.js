084dd0556707bbf4a5f9ef66f4ad20058ccde281
Jose Mato <matinho@gmail.com>
2022-02-21 16:03:11 +0100

    task1: Fix backend port

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [source, setSource] = useState(null);
 
   const fetchVideoData = async () => {
-    const rawVideoResponse = await fetch('http://localhost:30002/video/12345');
+    const rawVideoResponse = await fetch('http://localhost:3002/video/12345');
     const video = await rawVideoResponse.json();
 
     const rawTokenResponse = await fetch('http://localhost:3002/video/token', {
